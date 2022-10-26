9ce3dfd73bd173aa22840eaede4d96bf15170468
Natalya <nemia.me@gmail.com>
2022-10-27 01:42:12 +0300

    Домашняя работа

---
+++ HomeWork_02/Program.cs
@@ -0,0 +1,89 @@
+﻿// ДЗ 2
+
+/*
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int CutNumber(int number)
+{
+    number = number / 10 % 10;
+    return number;
+}
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = CutNumber(num);
+Console.WriteLine($"Второй цифрой числа {num} является {result}");
+*/
+
+/*
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int ThirdDigit(int number)
+{
+    if (number > 99)
+    {
+        number = number / 100 % 10;
+        return number;
+    }
+    else
+        return -1;
+}
+
+void WriteResult(int number, int result)
+{
+    if (result == -1)
+        Console.WriteLine("Третьей цифры нет.");
+    else
+        Console.WriteLine($"Третьей цифрой числа {number} является {result}.");
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = ThirdDigit(number);
+WriteResult(number, result);
+*/
+
+/*
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int ThirdDigit(int number)
+{
+    if(number < 100)
+        return -1;
+
+    else
+    {
+        while (number >= 1000)
+        {
+            number /= 10;
+        }
+        return number % 10;
+    }
+}
+
+void WriteResult(int number, int result)
+{
+    if (result == -1)
+        Console.WriteLine("Третьей цифры нет.");
+    else
+        Console.WriteLine($"Третьей цифрой числа {number} является {result}.");
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = ThirdDigit(number);
+WriteResult(number, result);
+*/
+
+/*
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+string Weekend(int number)
+{
+    if (number == 6 || number == 7)
+        return "Да";
+    if (number > 0 && number < 6)
+        return "Нет";
+    else
+        return "Введен неверный день недели.";
+}
+Console.Write("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Weekend(number));
+*/
---
+++ HomeWork_03/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+bool Palindrome(int num)
+
+{
+    bool result = true;
+    for (int i = 4; num > 10; i -= 2)
+    {
+        if (num / (Convert.ToInt32(Math.Pow(10, i))) != num % 10)
+        {
+            result = false;
+            break;
+        }
+        num = num % Convert.ToInt32(Math.Pow(10, i)) / 10;
+    }
+    return result;
+}
+
+void PrintResult(bool result)
+{
+    if (result == true) Console.WriteLine("Yes");
+    else Console.WriteLine("No");
+}
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Palindrome(number);
+
+PrintResult(result);
+*/
+
+/*
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Round((Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2))), 2);
+}
+
+Console.Write("Введите координату первой точки по оси Х: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату первой точки по оси Y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату первой точки по оси Z: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату второй точки по оси Х: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату второй точки по оси Y: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату второй точки по оси Z: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = Distance(x1,y1,z1,x2,y2,z2);
+Console.WriteLine(distance);
+*/
+
+/*
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void Cube (int num)
+
+{
+    for (int i = 1; i <= num; i++) Console.Write($"{Math.Pow(i,3)} ");
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Cube(num);
+*/
